a4d9bb0645fb14606139b313e4f57dd9c1984c8f
arthuraleks <arthuraleks@gmail.com>
2023-08-17 23:58:45 -0600

    mySQL init

--- server.js
+++ server.js
@@ -1,10 +1,38 @@
 var express = require("express");
 var app = express();
 
+// get the client
+const mysql = require('mysql2');
+
+
+
+
 app.get("/", function (req, res) {
-  res.send("Hola Mundo!");
+    //  conectamos 
+
+    // create the connection to database
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'classicmodels'
+    });
+
+    // simple query
+    connection.query(
+        'SELECT * FROM classicmodels.customers',
+         (err, results, fields) => {
+            console.log(results); // results contains rows returned by server
+            console.log(fields); // fields contains extra meta data about results, if available
+            res.send(results);
+        }
+    );
+
 });
 
 app.listen(3000, function () {
-  console.log("Aplicación ejemplo, escuchando el puerto 3000!");
+
+    
+
+    console.log("Aplicación ejemplo, escuchando el puerto 3000!");
 });
